81d499a50047930d6316e38801ea0f4e8621182e
Rongcui Dong <rongcuid@outlook.com>
2018-10-05 22:49:18 -0400

    Initial Commit

---
+++ Source/OpenSpaceOrSomething/OpenSpaceOrSomething.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenSpaceOrSomething.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OpenSpaceOrSomething, "OpenSpaceOrSomething" );
