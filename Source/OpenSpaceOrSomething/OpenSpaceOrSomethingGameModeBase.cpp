bb9a6b2cae8fd0cc4a4e5db7ccabd032d427fbf0
Olessya <olesyawixy@gmail.com>
2018-10-29 12:58:04 -0400

    Minor changes in the GameMode

--- Source/OpenSpaceOrSomething/OpenSpaceOrSomethingGameModeBase.cpp
+++ Source/OpenSpaceOrSomething/OpenSpaceOrSomethingGameModeBase.cpp
@@ -3,5 +3,16 @@
 #include "OpenSpaceOrSomethingGameModeBase.h"
 
 
+void AOpenSpaceOrSomethingGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	//ChangeWidget(MainWidget);
+}
 
-
+void AOpenSpaceOrSomethingGameModeBase::ChangeWidget(SMainWidget NewWidgetClass)
+{
+	//if current widget is not empty
+	//remove it from the view port
+	//if the new widget is not empty
+	//add the widget to the viewport
+}
